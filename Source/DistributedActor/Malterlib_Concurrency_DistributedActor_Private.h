27f7ebb52bb848bd8ebdad69b99d1492a0e850d8
Erik Olofsson <erik.olofsson@hansoft.com>
2017-01-10 13:01:21 +0000

    [Malterlib/Concurrency] Fix integration error
    Copied from Perforce
     Change: 81363

---
+++ Source/DistributedActor/Malterlib_Concurrency_DistributedActor_Private.h
@@ -0,0 +1,20 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+namespace NMib::NConcurrency::NPrivate
+{
+	struct CDistributedActorData : public ICDistributedActorData
+	{
+		bool f_IsValidForCall() const override;
+		
+		TCWeakActor<CActorDistributionManager> m_DistributionManager;
+		NStr::CStr m_ActorID;
+		NPtr::TCWeakPointer<NPrivate::ICHost> m_pHost;		
+		uint32 m_ProtocolVersion = TCLimitsInt<uint32>::mc_Max;
+		bool m_bWasDestroyed = false;
+		bool m_bRemote = false;
+	};
+	
+}
---
+++ Source/DistributedActor/Malterlib_Concurrency_DistributedActor_Private.hpp
@@ -0,0 +1,38 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+namespace NMib::NConcurrency::NPrivate
+{
+	template <typename tf_CFirst>
+	void fg_GetDistributedActorInheritanceHierarchy(NContainer::TCVector<uint32> &_Settings)
+	{
+		_Settings.f_Insert(fg_GetTypeHash<tf_CFirst>());
+		fg_GetDistributedActorInheritanceHierarchy<typename NTraits::TCGetBase<tf_CFirst>::CType>(_Settings);
+	}
+	
+	template <>
+	inline_always_debug void fg_GetDistributedActorInheritanceHierarchy<CActor>(NContainer::TCVector<uint32> &_Settings)
+	{
+	}
+
+	struct CSubSystem_Concurrency_DistributedActor : public CSubSystem
+	{
+		CSubSystem_Concurrency_DistributedActor();
+		~CSubSystem_Concurrency_DistributedActor();
+
+		struct CThreadLocal
+		{
+			CCallingHostInfo m_CallingHostInfo;
+			CDistributedActorData *m_pCurrentRemoteDispatchActorData = nullptr;
+			NStr::CStr m_SuggestedEnclave;
+		};
+		
+		NThread::TCThreadLocal<CThreadLocal> m_ThreadLocal;
+	};
+	
+	CSubSystem_Concurrency_DistributedActor &fg_DistributedActorSubSystem();
+}
+
+#include "Malterlib_Concurrency_DistributedActor_Stream_Private.hpp"

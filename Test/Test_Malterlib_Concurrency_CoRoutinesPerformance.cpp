1a9078fcbb91968d8237a93cc599c6d5477f38d3
Erik Olofsson <erik@olofsson.info>
2023-04-30 13:28:46 +0200

    [Malterlib/Concurrency] Add performance tests for sync co-routines

---
+++ Test/Test_Malterlib_Concurrency_CoRoutinesPerformance.cpp
@@ -0,0 +1,107 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Concurrency/ConcurrencyManager>
+#include <Mib/Concurrency/Coroutine>
+#include <Mib/Test/Performance>
+
+#ifdef DCompiler_clang
+#pragma clang diagnostic ignored "-Walways-inline-coroutine"
+#endif
+
+namespace NMib::NConcurrency::NTest
+{
+	using namespace NMib;
+	using namespace NMib::NStorage;
+	using namespace NMib::NMeta;
+	using namespace NMib::NContainer;
+	using namespace NMib::NConcurrency;
+	using namespace NMib::NThread;
+	using namespace NMib::NFunction;
+	using namespace NMib::NMemory;
+
+
+	class CCoroutinesPerformance_Tests : public NMib::NTest::CTest
+	{
+		constexpr static mint mc_nLoops = 16384 * 128;
+
+		static inline_never uint32 fs_TestRecursive(uint32 _Value)
+		{
+			volatile uint32 Result = _Value * 2;
+			return Result;
+		}
+
+		static inline_never uint32 fs_TestFunction(uint32 _Value)
+		{
+			volatile uint32 Return = 0;
+			for (mint i = 0; i < mc_nLoops; ++i)
+				Return = Return + fs_TestRecursive(_Value);
+			return Return;
+		}
+
+		static inline_always TCFuture<uint32> fs_TestRecursiveCoro(uint32 _Value)
+		{
+			co_return _Value * 2;
+		}
+
+		static inline_never TCFuture<uint32> fs_TestFunctionCoro(uint32 _Value)
+		{
+			uint32 Return = 0;
+			for (mint i = 0; i < mc_nLoops; ++i)
+				Return += co_await fs_TestRecursiveCoro(_Value);
+			co_return Return;
+		}
+
+		void f_DoTests()
+		{
+			DMibTestSuite(CTestCategory("Synchronous") << CTestGroup("Performance")) -> TCFuture<void>
+			{
+				mint nTests = 9;
+
+				CTestPerformanceMeasure NormalTime("Normal");
+				CTestPerformanceMeasure CoroutineTime("Coroutine");
+				uint32 ReturnNormal;
+				uint32 ReturnCoro;
+				{
+					for(mint j = 0; j < nTests; ++j)
+					{
+						NormalTime.f_Start();
+						uint32 Multiplier = 2;
+						[&Multiplier, &ReturnNormal]() inline_never
+							{
+								ReturnNormal = fs_TestFunction(Multiplier);
+							}
+							()
+						;
+						NormalTime.f_Stop(mc_nLoops);
+					}
+				}
+				{
+					for(mint j = 0; j < nTests; ++j)
+					{
+						CoroutineTime.f_Start();
+						uint32 Multiplier = 2;
+						[&Multiplier, &ReturnCoro]() inline_never
+							{
+								ReturnCoro = fs_TestFunctionCoro(Multiplier).f_CallSync();
+							}
+							()
+						;
+						CoroutineTime.f_Stop(mc_nLoops);
+					}
+				}
+
+				DMibExpect(ReturnNormal, ==, ReturnCoro);
+
+				CTestPerformance PerfTest(0.015);
+				PerfTest.f_AddReference(NormalTime);
+				PerfTest.f_Add(CoroutineTime);
+				DMibExpectTrue(PerfTest);
+
+				co_return {};
+			};
+		}
+	};
+
+	DMibTestRegister(CCoroutinesPerformance_Tests, Malterlib::Concurrency);
+}

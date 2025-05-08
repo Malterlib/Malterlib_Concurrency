4528b72462b6ee570815ca686ee5f25ebf85d116
Erik Olofsson <erik@olofsson.info>
2025-05-08 12:48:16 +0200

    [Malterlib/Concurrency] Remove debug code

--- Source/DistributedApp/Malterlib_Concurrency_DistributedUIApp_MacOS.mm
+++ Source/DistributedApp/Malterlib_Concurrency_DistributedUIApp_MacOS.mm
@@ -11,28 +11,8 @@
 
 namespace NMib::NConcurrency
 {
-	struct CTesting
-	{
-		TCFuture<NContainer::CIOByteVector> f_Test(NStorage::TCSharedPointer<NPrivate::CDistributedActorData>, NContainer::CIOByteVector, NPrivate::CDistributedActorStreamContext);
-	};
-
 	void CDistributedUIApp::fsp_RunMain()
 	{
-		CTesting Testing;
-		NPrivate::CDistributedActorStreamContext Temp (5, true);
-		(void)Testing.f_Test(NStorage::TCSharedPointer<NPrivate::CDistributedActorData>(), NContainer::CIOByteVector(), Temp);
-
-
-		static_assert
-		(
-			NTraits::TCIsCallableWith
-			<
-				TCFuture<NContainer::CIOByteVector> (*)(NStorage::TCSharedPointer<NPrivate::CDistributedActorData>, NContainer::CIOByteVector, NPrivate::CDistributedActorStreamContext)
-				, void (NStorage::TCSharedPointer<NPrivate::CDistributedActorData>, NContainer::CIOByteVector, NPrivate::CDistributedActorStreamContext)
-			>::mc_Value
-		);
-
-
 		ch8	const *Dummy[] = {nullptr};
 		NSApplicationMain(0, Dummy); // Will never return
 	}
